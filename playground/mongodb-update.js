f79cb899bf617466648be41a93e1c6b9b1eedec9
mindtouchben <benr@mindtouch.com>
2017-09-25 16:23:04 -0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,26 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59c977583ce3191110a0a22d')
+  }, {
+    $set: {
+      name: 'Ben'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result.value);
+  });
+
+  // db.close();
+});
